276e3c61649892983e442f59dae86bb1fa601ce8
clopq <clopq@DESKTOP-3DIA8P2>
2024-10-11 12:12:00 +0200

    Primer Commit con la clase Calculadora y su Test

---
+++ src/TDD/Calculadora.java
@@ -0,0 +1,19 @@
+package TDD;
+
+public class Calculadora {
+	public static int resta(int i, int j) {
+        return i-j;
+    }
+
+    public static int multiplica(int i, int j) {
+        return i*j;
+    }
+
+    public static int suma(int i, int j) {
+        return i+j;
+    }
+
+    public static int divide(int i, int j) {
+        return i/j;
+    }
+}
